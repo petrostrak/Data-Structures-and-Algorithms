dbf45106cec35f2c7915458f378ffaddc6d9d092
petros <pit.trak@gmail.com>
2020-06-19 10:38:34 +0300

    Linear Search

---
+++ Searching Algorithms/Linear Search/Example #1/src/model/LinearSearch.java
@@ -0,0 +1,13 @@
+package model;
+
+public class LinearSearch {
+	
+	public static int linearSearch(int arr[], int x) {
+		for(int i = 0; i < arr.length; i ++) {
+			if(arr[i] == x) {
+				return i;
+			}
+		}
+		return -1;
+	}
+}
---
+++ Searching Algorithms/Linear Search/Example #1/src/model/Main.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int arr[] = {4, 8, 10, 22, 34, 60, 1, 0};
+		int x = 22;
+		int result = LinearSearch.linearSearch(arr, x);
+		System.out.println(result);
+		
+	}
+
+}
