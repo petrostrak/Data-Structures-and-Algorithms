957f1adb8185883cf4951e4cf2d8c63f8d49b6f0
petros <pit.trak@gmail.com>
2020-08-09 14:59:10 +0300

    Theory about Singly LinkedLists and Doubly LinkedLists

--- Data Structures/Collections/LinkedLists/src/linkedList/App.java
+++ Data Structures/Collections/LinkedLists/src/linkedList/App.java
@@ -1,7 +1,22 @@
 package linkedList;
 
 public class App {
-
+/*
+ *  It is another implementation of the List interface
+ *  		- implements List, Queue, Deque interfaces 
+ *  It uses references -> eachitem has a reference to the next item + stores the data itself
+ *  
+ *  			43  -> 100  -> -12  -> 1 -> NULL	Singly Linked List
+ *  	NULL <-	43 <-> 100 <-> -12 <-> 1 -> NULL	Doubly Linked List
+ *  
+ *  - NO RANDOM ACCESS !!!
+ *  - Not synchronized
+ *  - We can remove items very efficiently / O(1)
+ *  - No sequential access !!! If we find an item we have to iterate through the list  until we find it / O(n)
+ *  - Remove operation is very efficient: Just update the references / pointers O(1)
+ *  - We should use a linkedList for remove heavy applications !
+ *  
+ */
 	public static void main(String[] args) {
 		List<Integer> list = new LinkedList<Integer>();
 
