cb353ba95c56fc06b3a87a3b33a24a102e73051f
petros <pit.trak@gmail.com>
2020-08-09 16:17:58 +0300

    Stacks

---
+++ Data Structures/Collections/Stack/src/stack/App.java
@@ -0,0 +1,21 @@
+package stack;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		Stack<Integer> myStack = new Stack<>();
+		
+		myStack.push(10);
+		myStack.push(100);
+		myStack.push(1000);
+		
+		System.out.println(myStack.size());
+		
+		// Last In First Out
+		System.out.println(myStack.pop());
+		System.out.println(myStack.pop());
+		System.out.println(myStack.pop());
+	}
+
+}
---
+++ Data Structures/Collections/Stack/src/stack/Stack.java
@@ -0,0 +1,44 @@
+package stack;
+
+public class Stack<T extends Comparable<T>> {
+	
+	private Node<T> root;
+	private int count;
+	
+	// O(1) constant time complexity
+	public void push(T newData) {
+		this.count ++;
+		
+		if(this.root == null) {
+			this.root = new Node<>(newData);
+		} else {
+			Node<T> oldRoot = this.root;
+			this.root = new Node<>(newData);
+			this.root.setNextNode(oldRoot); // Updating reference
+		}
+	}
+	
+	// O(1) constant time complexity
+	public T pop() {
+		T itemToPop = this.root.getData();
+		this.root = this.root.getNextNode(); // Updating reference
+		this.count--;
+		return itemToPop;
+	}
+	
+	// O(1) constant time complexity
+	public T peek() {
+		return this.root.getData();
+	}
+	
+	// O(1) constant time complexity
+	public int size() {
+		return this.count;
+	}
+	
+	// O(1) constant time complexity
+	public boolean isEmplty() {
+		return this.root == null;
+	}
+
+}
